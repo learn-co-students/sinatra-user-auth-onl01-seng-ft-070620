c3670d7d0f1f490b867def3e07fc2d1c9498f9f8
Ethan <eleetyson@gmail.com>
2020-08-19 09:37:27 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,13 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+# renders the sign-up form
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
+# handles the POST request from the user sign-up form
+# creates a new user, signs them in, and redirects them
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
@@ -24,12 +26,13 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
+# renders the login form
   get '/sessions/login' do
-
-    # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
+# handles the POST request from user login
+# signs the user in
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
@@ -39,13 +42,14 @@ class ApplicationController < Sinatra::Base
     redirect '/sessions/login'
   end
 
+# clears the session hash and logs the user out
   get '/sessions/logout' do
     session.clear
     redirect '/'
   end
 
+# renders the user's homepage
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
