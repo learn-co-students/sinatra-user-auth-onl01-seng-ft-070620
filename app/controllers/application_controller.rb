56d13611507ba3d102902e700784d6f5df93564f
Amanda_Katherine <walker.akw@gmail.com>
2020-08-20 14:54:37 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    # @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    # @user.save
     @user = User.create(name: params["name"], email: params["email"], password: params["password"])
     session[:user_id] = @user.id
 
